f00df564ad3e39e061b951d9d2deaf6217a52a14
Aleksey Sakalinski <aos-1995@yandex.com>
2023-12-20 23:17:42 +0300

    Change CartViewComponent

--- LAB_1/Components/CartViewComponent.cs
+++ LAB_1/Components/CartViewComponent.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WEB.Extensions;
+using WEB.Models;
 
 namespace WEB.Components
 {
     public class CartViewComponent: ViewComponent
     {
-        public IViewComponentResult Invoke() => View();
-        
+        public IViewComponentResult Invoke()
+        {
+            var cart = HttpContext.Session.Get<Cart>("cart");
+            return View(cart);
+        }
+
     }
 }
