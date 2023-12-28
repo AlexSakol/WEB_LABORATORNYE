163c7cac0cee8aa14967eaaa006eb1fdb020cec4
Aleksey Sakalinski <aos-1995@yandex.com>
2023-12-28 21:48:39 +0300

    Delete unusing namespace in Program

--- LAB_1/Program.cs
+++ LAB_1/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using WEB.Data;
 using WEB.Extensions;
 using WEB.Models;
 using WEB.Services;
